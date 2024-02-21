91ed936a1190f6e1c6c51b999882d70ffb53ad64
hoit1011 <parksm071011@gmail.com>
2024-02-22 04:34:48 +0900

    test

---
+++ app/src/main/java/com/example/kotlin_and_db/helloActivity.kt
@@ -0,0 +1,23 @@
+// helloActivity.kt
+package com.example.kotlin_and_db
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlin_and_db.databinding.ActivityHelloBinding
+
+class HelloActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHelloBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHelloBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.text.text = "Hello"
+
+        binding.button.setOnClickListener{
+            binding.text.text = "click!"
+        }
+    }
+
+}
